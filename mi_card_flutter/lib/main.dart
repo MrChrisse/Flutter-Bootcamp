714cf1256b95e85d57ea4fee45c0a66d871940af
Christian Leingang <chrilei2000@gmail.com>
2021-12-11 22:48:26 +0100

    finished chapter6

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -10,7 +10,116 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.red,
+              foregroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"),
+            ),
+            Text(
+              "Christian",
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'),
+            ),
+            Text(
+              "Flutter Developer".toUpperCase(),
+              style: TextStyle(
+                color: Colors.teal.shade100,
+                fontFamily: 'SourceSans',
+                fontSize: 20,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 70,
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                  size: 20,
+                ),
+                title: Text(
+                  "+ 49 1432 113343",
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans',
+                      fontSize: 20),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                  size: 20,
+                ),
+                title: Text(
+                  "test@mail.de",
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans',
+                      fontSize: 20),
+                ),
+              ),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+class LayoutChallenge extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 100,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Container(
+              width: 100,
+              color: Colors.blue,
+            )
+          ],
+        ),
       ),
     );
   }
